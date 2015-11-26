6409596fdc0d36c7c126316fbb92a11ab43b8287
Krushan <krushan1795@gmail.com>
2015-11-25 19:41:38 -0500

    ELFANDHALFELF

---
+++ src/Models/Races/HalfElf.java
@@ -0,0 +1,14 @@
+import import java.io.Serializable;
+
+/**
+ * Created by krushan on 11/23/15.
+ */
+public class HalfElf extends Race implements Serializable{
+    @Override
+    public void modifyAttributes(Attributes attributes) {
+
+    }
+    public void modifyAttribute(Attribute attribute){
+        
+    }
+}
