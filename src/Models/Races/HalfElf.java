f260d7843182675cc2cd065ab86808d838c60468
Krushan1 <krushan1795@gmail.com>
2015-11-27 10:30:27 -0500

    UpdatedHalf-Elf

--- src/Models/Races/HalfElf.java
+++ src/Models/Races/HalfElf.java
@@ -15,12 +15,67 @@ import java.io.Serializable;
  */
 public class HalfElf extends Race implements Serializable {
 
-    @Override
-    public void modifyAttributes(Attributes attributes) {
+    public class HalfElf extends Race implements Serializable
+{
 
+    public enum AbilityBoost
+    {
+
+        STRENGTH, DEXTERITY, CONSTITUTION, INTELLIGENCE, WISDOM, CHARISMA;
     }
 
-    public void modifyAttribute(Attribute attribute) {
+    private AbilityBoost abilityBoost;
 
+    public HalfElf(AbilityBoost abilityBoost)
+    {
+        this.abilityBoost = abilityBoost;
+    }
+
+    @Override
+    public void modifyAttributes(Attributes attributes)
+    {
+        switch (abilityBoost)
+        {
+            case STRENGTH:
+            {
+                Attribute str = attributes.getStrength();
+                str.setAbilityScore((str.getAbilityScore() + 2));
+                break;
+            }
+            case DEXTERITY:
+            {
+                Attribute dex = attributes.getDexterity();
+                dex.setAbilityScore((dex.getAbilityScore() + 2));
+                break;
+            }
+            case CONSTITUTION:
+            {
+                Attribute con = attributes.getConstitution();
+                con.setAbilityScore((con.getAbilityScore() + 2));
+                break;
+            }
+            case INTELLIGENCE:
+            {
+                Attribute intl = attributes.getIntelligence();
+                intl.setAbilityScore((intl.getAbilityScore() + 2));
+                break;
+            }
+            case WISDOM:
+            {
+                Attribute wis = attributes.getWisdom();
+                wis.setAbilityScore((wis.getAbilityScore() + 2));
+                break;
+            }
+            case CHARISMA:
+            {
+                Attribute cha = attributes.getCharisma();
+                cha.setAbilityScore((cha.getAbilityScore() + 2));
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
     }
 }
